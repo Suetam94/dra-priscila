195387586bb8b9c0b0312bb5b61af64c633613fd
Suetam94 <mateusviniciusdasilva@outlook.com>
2024-08-02 10:26:57 -0300

    feat: interface: header

--- app/ui/general/menu-mobile.tsx
+++ app/ui/general/menu-mobile.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from 'react'
 import { List, X } from '@phosphor-icons/react'
 import clsx from 'clsx'
+import Link from 'next/link'
 
 const MobileMenu = (): React.JSX.Element => {
   const [isOpen, setIsOpen] = useState(false)
@@ -36,11 +37,11 @@ const MobileMenu = (): React.JSX.Element => {
           </button>
         </div>
         <div className="mt-16 flex flex-col items-center space-y-4 p-4 w-full">
-          <a href="/quem-sou" className="block text-xl px-4 py-2 rounded-md hover:bg-base-blue hover:text-white transition-colors duration-300" onClick={closeMenu}>Quem Sou</a>
-          <a href="/areas-de-atuacao" className="block text-xl px-4 py-2 rounded-md hover:bg-base-blue hover:text-white transition-colors duration-300" onClick={closeMenu}>Áreas de Atuação</a>
-          <a href="/onde-pode-me-encontrar" className="block text-xl px-4 py-2 rounded-md hover:bg-base-blue hover:text-white transition-colors duration-300" onClick={closeMenu}>Onde Pode Me Encontrar</a>
-          <a href="/mais-sobre-a-dermatologia" className="block text-xl px-4 py-2 rounded-md hover:bg-base-blue hover:text-white transition-colors duration-300" onClick={closeMenu}>Saiba Mais Sobre a Dermatologia</a>
-          <a href="/marque-sua-consulta" className="block text-xl px-4 py-2 rounded-md hover:bg-base-blue hover:text-white transition-colors duration-300" onClick={closeMenu}>Marque a Sua Consulta</a>
+          <Link href="/quem-sou" className="block text-xl px-4 py-2 rounded-md hover:bg-base-blue hover:text-white transition-colors duration-300" onClick={closeMenu}>Quem Sou</Link>
+          <Link href="/areas-de-atuacao" className="block text-xl px-4 py-2 rounded-md hover:bg-base-blue hover:text-white transition-colors duration-300" onClick={closeMenu}>Áreas de Atuação</Link>
+          <Link href="/onde-pode-me-encontrar" className="block text-xl px-4 py-2 rounded-md hover:bg-base-blue hover:text-white transition-colors duration-300" onClick={closeMenu}>Onde Pode Me Encontrar</Link>
+          <Link href="/mais-sobre-a-dermatologia" className="block text-xl px-4 py-2 rounded-md hover:bg-base-blue hover:text-white transition-colors duration-300" onClick={closeMenu}>Saiba Mais Sobre a Dermatologia</Link>
+          <Link href="/marque-sua-consulta" className="block text-xl px-4 py-2 rounded-md hover:bg-base-blue hover:text-white transition-colors duration-300" onClick={closeMenu}>Marque a Sua Consulta</Link>
         </div>
       </div>
     </div>
