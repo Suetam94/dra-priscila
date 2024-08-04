fe46adc969234d751e674cbf34273e55427d6364
Suetam94 <mateusviniciusdasilva@outlook.com>
2024-08-04 19:14:58 -0300

    feat: interface: link into the header for back to home page

--- app/ui/general/header.tsx
+++ app/ui/general/header.tsx
@@ -10,7 +10,9 @@ const Header = (): React.JSX.Element => {
   return (
     <header className="w-full relative bg-base-blue">
       <div className="max-w-7xl mx-auto flex justify-between items-center p-2 lg:px-8 lg:gap-10">
-        <Image src="/logo-header.png" alt="Dra. Priscila Dermatologista" width={190} height={70} />
+        <Link className="pl-2" href="/">
+          <Image src="/logo-header.png" alt="Dra. Priscila Dermatologista" width={190} height={70} />
+        </Link>
         <div className="hidden lg:flex lg:justify-between lg:gap-7 items-center text-center font-bold">
           <Link href="/quem-sou" className="text-base-gray hover:text-white transition-colors duration-300">Quem Sou</Link>
           <Link href="/areas-de-atuacao" className="text-base-gray hover:text-white transition-colors duration-300">Áreas de Atuação</Link>
