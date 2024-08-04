c1bdc54ea21e4381590b581df3f62c8fcaad1ec1
Suetam94 <mateusviniciusdasilva@outlook.com>
2024-08-04 19:48:29 -0300

    fix: interface: flex shrink

--- app/ui/book-your-appointment/clinic.tsx
+++ app/ui/book-your-appointment/clinic.tsx
@@ -33,7 +33,7 @@ const Clinic = ({ name, image, contact, address }: IClinicProps) => {
   }
 
   return (
-    <div key={name} className="flex flex-col md:flex-row items-center mb-5 bg-white rounded-lg shadow-lg p-4 h-[350px]">
+    <div key={name} className="flex flex-col md:flex-row items-center mb-5 bg-white rounded-lg shadow-lg p-4 h-[500px] md:h-[350px]">
       <div className="w-full md:w-1/2 p-4 flex justify-center">
         <div className="w-72 md:w-128 h-auto">
           <Image
@@ -47,24 +47,24 @@ const Clinic = ({ name, image, contact, address }: IClinicProps) => {
           />
         </div>
       </div>
-      <div className="w-full md:w-1/2 p-4 text-center md:text-left flex flex-col justify-start">
+      <div className="w-full md:w-1/2 p-4 text-center md:text-left flex flex-col justify-start flex-shrink-0">
         <h3 className="text-2xl font-bold text-base-blue mb-4">{name}</h3>
-        <div className="flex items-center text-base-blue text-lg font-medium mb-2">
-          <MapPin className="w-6 h-6 text-base-blue mr-2" />
+        <div className="flex items-center text-base-blue text-lg font-medium mb-2 flex-shrink-0">
+          <MapPin className="w-6 h-6 text-base-blue mr-2 flex-shrink-0" />
           <span>{address}</span>
         </div>
         {contact.phones.map((phone) => (
-          <div key={phone.number} className="text-base-blue text-lg font-medium mb-2 flex items-center">
+          <div key={phone.number} className="text-base-blue text-lg font-medium mb-2 flex items-center flex-shrink-0">
             {phone.isWhatsapp
               ? (
               <a href={getWhatsappLink(phone.ddd, phone.number)} target="_blank" rel="noopener noreferrer" className="flex items-center">
-                <WhatsappLogo className="w-6 h-6 text-base-pink mr-2" />
+                <WhatsappLogo className="w-6 h-6 text-base-pink mr-2 flex-shrink-0" />
                 <span className="text-base-pink">{formatPhoneNumber(phone.ddd, phone.number)}</span>
               </a>
                 )
               : (
-              <div className="flex items-center">
-                <Phone className="w-6 h-6 text-base-blue mr-2" />
+              <div className="flex items-center flex-shrink-0">
+                <Phone className="w-6 h-6 text-base-blue mr-2 flex-shrink-0" />
                 <span>{formatPhoneNumber(phone.ddd, phone.number)}</span>
               </div>
                 )}
